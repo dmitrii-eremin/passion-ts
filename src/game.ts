bb97a9ed4621a31d33ec8dacf900981c50bd848c
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-05-16 19:31:27 +0300

    Added btn

--- src/game.ts
+++ src/game.ts
@@ -1,24 +1,65 @@
 import { Colors } from './passion/color';
 import type { Passion } from './passion/passion';
 
+class Player {
+    private passion: Passion;
+
+    playerX: number = 0;
+    playerY: number = 0;
+    speed: number = 100;
+
+    constructor(passion: Passion, x: number, y: number) {
+        this.passion = passion;
+        this.playerX = x;
+        this.playerY = y;
+    }
+
+    update(dt: number) {
+        
+    }
+
+    move(dx: number, dy: number, dt: number) {
+        this.playerX += dx * this.speed * dt;
+        this.playerY += dy * this.speed * dt;
+    }
+
+    draw() {
+        this.passion.graphics.circb(Math.ceil(this.playerX), Math.ceil(this.playerY), 3, Colors[12]);
+    }
+}
+
 export class Game {
     private passion: Passion;
-    private deltaTime: number = 0;
+
+    private player: Player;
 
     constructor(passion: Passion) {
         this.passion = passion;
+        this.player = new Player(passion, 120, 90);
+
         this.passion.system.init(240, 180, 'A demo game');
     }
 
     update(dt: number) {
-        this.deltaTime = dt;
+        if (this.passion.input.btn('ArrowUp')) {
+            this.player.move(0, -1, dt);
+        }
+        if (this.passion.input.btn('ArrowDown')) {
+            this.player.move(0, 1, dt);
+        }
+        if (this.passion.input.btn('ArrowLeft')) {
+            this.player.move(-1, 0, dt);
+        }
+        if (this.passion.input.btn('ArrowRight')) {
+            this.player.move(1, 0, dt);
+        }
+
+        this.player.update(dt);
     }
 
     draw() {
         this.passion.graphics.cls(Colors[1]);
 
-        this.passion.graphics.text(2, 2, `Window size: 240x180`, Colors[7]);
-        this.passion.graphics.text(2, 12, `dt: ${this.deltaTime}`, Colors[15]);
-        this.passion.graphics.text(2, 24, `Mouse: (${Math.floor(this.passion.input.mouse_x)}, ${Math.floor(this.passion.input.mouse_y)})`, Colors[7]);
+        this.player.draw();
     }
 }
