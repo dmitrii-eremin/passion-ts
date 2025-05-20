84ce61e7502ef92b4f02c5323768c5c3d0192ed9
Dmitrii Eremin <erem.dmitrii@gmail.com>
2025-05-20 11:46:37 +0300

    Improved network game

--- src/game/server.ts
+++ src/game/server.ts
@@ -6,26 +6,38 @@ export type Command = {
     type: CommandType;
 };
 
-export type CommandLogin = Command & {
-    username: string;
-};
-
 export type CommandSetPos = Command & {
     x: number;
     y: number;
+};
+
+export type CommandLogin = Command & CommandSetPos & {
+    username: string;
 };
 
 export class ObjectData {
     name: string = '';
     x: number = 0;
     y: number = 0;
+
+    constructor(name: string = '', x: number = 0, y: number = 0) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
 }
 
 export type CommandUpdate = Command & {
     objects: ObjectData[];
+    cats: ObjectData[];
 };
 
 const activePlayers: Map<WebSocket, ObjectData> = new Map<WebSocket, ObjectData>();
+const cats: ObjectData[] = [];
+
+for (let i = 0; i < 25; i++) {
+    cats.push(new ObjectData('kissa', Math.floor(Math.random() * 400) - 200, Math.floor(Math.random() * 400) - 200));
+}
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -41,6 +53,8 @@ wss.on('connection', ws => {
                 const cmd: CommandLogin = message as CommandLogin;
                 console.log(`Registered player: "${cmd.username}"`)
                 activePlayers.get(ws)!.name = cmd.username;
+                activePlayers.get(ws)!.x = cmd.x;
+                activePlayers.get(ws)!.y = cmd.y;
             }
             else if (message.type === 'set_pos') {
                 const cmd: CommandSetPos = message as CommandSetPos;
@@ -50,19 +64,23 @@ wss.on('connection', ws => {
             }
         }
 
+        const updateData: CommandUpdate = {
+            type: 'update',
+            objects: Array.from(activePlayers.values()),
+            cats
+        };
+
+        console.log(`Players count: ${updateData.objects.length}`);
         wss.clients.forEach((client: WebSocket) => {
             if (client.readyState === ws.OPEN) {
-                const data: CommandUpdate = {
-                    type: 'update',
-                    objects: Array.from(activePlayers.values())
-                };
-                client.send(JSON.stringify(data));
+                client.send(JSON.stringify(updateData));
             }
         });
     });
 
     ws.on('close', () => {
         console.log('Client disconnected');
+        activePlayers.delete(ws);
     });
 });
 
