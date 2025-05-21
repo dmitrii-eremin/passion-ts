9a6661a0d251575ad4fafb2cd2447faa5c21846a
Dmitrii, Eremin <erem.dmitrii@gmail.com>
2025-05-21 22:39:18 +0300

    added demo

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,45 +1,65 @@
 import { type ImageIndex } from "../passion/constants";
+import { generateUniqueName } from "../passion/internal/random_id";
 import type { Passion } from "../passion/passion";
 import { Bump, World } from '../passion/stdlib/bump/index';
 import { Position } from "../passion/stdlib/position";
 
 class Cat {
     private passion: Passion;
+    private world: World;
+
     private catId: ImageIndex;
 
-    private catPos: Position; 
-    private catDirection: Position;
-    private catSpeed: number = 50;
+    private uniqueId: string = generateUniqueName();
 
-    constructor(passion: Passion, catId: ImageIndex) {
+    private pos: Position; 
+    private direction: Position;
+    private readonly speed: number = 50;
+    private size: Position = Position.fromCoords(16, 16);
+
+    constructor(passion: Passion, world: World, catId: ImageIndex) {
         this.passion = passion;
+        this.world = world;
+
         this.catId = catId;
-        this.catPos = Position.fromCoords(
-            this.passion.math.rndi(0, this.passion.system.width - 16),
-            this.passion.math.rndi(0, this.passion.system.height - 16),
+        this.pos = Position.fromCoords(
+            this.passion.math.rndi(0, this.passion.system.width - this.size.x),
+            this.passion.math.rndi(0, this.passion.system.height - this.size.y),
         );
-        this.catDirection = Position.random();
+        this.direction = Position.random();
+
+        this.world.add(this.uniqueId, this.pos.x, this.pos.y, this.size.x, this.size.y)
     }
 
     update(dt: number) {
-        this.catPos = this.catPos.add(this.catDirection.multiple(this.catSpeed * dt));
+        const goal = this.pos.add(this.direction.multiple(this.speed * dt));
+        const actual = this.world.move(this.uniqueId, goal.x, goal.y);
+        this.pos = Position.fromCoords(actual.x, actual.y);
+        for (const col of actual.collisions) {
+            if (col.normal.x !== 0) {
+                this.direction.x = -this.direction.x;
+            }
+            if (col.normal.y !== 0) {
+                this.direction.y = -this.direction.y;
+            }
+        }
 
         if (
-            (this.catPos.x < 0 && this.catDirection.x < 0) ||
-            (this.catPos.x > this.passion.system.width - 16 && this.catDirection.x > 0)
+            (this.pos.x < 0 && this.direction.x < 0) ||
+            (this.pos.x > this.passion.system.width - 16 && this.direction.x > 0)
         ) {
-            this.catDirection.x = -this.catDirection.x;
+            this.direction.x = -this.direction.x;
         }
         if (
-            (this.catPos.y < 0 && this.catDirection.y < 0) ||
-            (this.catPos.y > this.passion.system.height - 16 && this.catDirection.y > 0)
+            (this.pos.y < 0 && this.direction.y < 0) ||
+            (this.pos.y > this.passion.system.height - 16 && this.direction.y > 0)
         ) {
-            this.catDirection.y = -this.catDirection.y;
+            this.direction.y = -this.direction.y;
         }
     }
 
     draw() {
-        this.passion.graphics.blt(Math.ceil(this.catPos.x), Math.ceil(this.catPos.y), this.catId, 0, 0, 16, 16);
+        this.passion.graphics.blt(Math.ceil(this.pos.x), Math.ceil(this.pos.y), this.catId, 0, 0, 16, 16);
     }
 }
 
@@ -54,10 +74,8 @@ export class Game {
             this.world = Bump.newWorld(16);
             this.passion.system.init(240, 180, 'A demo game');
 
-            this.world.add('dasasxasd', -100, -100, 1, 1);
-
             const catId = this.passion.resource.loadImage('./cat_16x16.png');
-            this.cats = Array.from({ length: 15 }, () => new Cat(this.passion, catId));
+            this.cats = Array.from({ length: 15 }, () => new Cat(this.passion, this.world, catId));
     }
 
     update(dt: number) {
