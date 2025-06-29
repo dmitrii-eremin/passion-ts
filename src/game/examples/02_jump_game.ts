284b41a70d0159381c10be05b5f3471dcfba0286
Dmitrii Eremin <erem.dmitrii@gmail.com>
2025-06-30 01:26:54 +0300

    added example02

---
+++ src/game/examples/02_jump_game.ts
@@ -0,0 +1,239 @@
+import type { Passion } from "../../passion/passion";
+import { Animation, AnimationGrid } from "../../passion/stdlib/animation";
+import { Position } from "../../passion/stdlib/position";
+import type { IGameExample } from "./example";
+
+const GRAVITY = 25;
+const FLOOR_LEVEL = 120;
+const GROUND_SPEED = 75;
+const BACKGROUND_SPEED = 25;
+
+interface MaterialObject {
+    readonly pos: Position;
+    readonly size: Position;
+}
+
+class Player implements MaterialObject {
+    private passion: Passion;
+
+    private imageId: string = '';
+    private animation: Animation;
+
+    private speedY: number = 0;
+
+    private readonly jumpSpeed = 8;
+
+    pos: Position = new Position(30, 45);
+    size: Position = new Position(16, 16);
+
+    constructor(passion: Passion) {
+        this.passion = passion;
+
+        this.imageId = passion.resource.loadImage('/examples/hero.png');
+
+        const grid = new AnimationGrid(this.size.x, this.size.y);
+        this.animation = new Animation(grid.range('2-6', '3'), 0.075);
+    }
+
+    update(dt: number) {
+        this.animation.update(dt);
+
+        if (this.pos.y >= FLOOR_LEVEL - this.size.y - 1 && this.speedY >= 0) {
+            if (this.passion.input.btnp('Space') || this.passion.input.btnp('MouseButtonLeft')) {
+                this.speedY = -this.jumpSpeed;
+            }
+        }
+
+        this.speedY += GRAVITY * dt;
+        this.pos.y += this.speedY;
+        if (this.pos.y >= FLOOR_LEVEL - this.size.y) {
+            this.pos.y = FLOOR_LEVEL - this.size.y;
+            this.speedY = 0;
+        }
+    }
+
+    draw() {
+        this.animation.draw(this.passion, Math.ceil(this.pos.x), Math.ceil(this.pos.y), this.imageId);
+    }
+
+    isCollidedWith(obj: MaterialObject): boolean {
+        return (
+            this.pos.x < obj.pos.x + obj.size.x &&
+            this.pos.x + this.size.x > obj.pos.x &&
+            this.pos.y < obj.pos.y + obj.size.y &&
+            this.pos.y + this.size.y > obj.pos.y
+        );
+    }
+}
+
+class Background {
+    private passion: Passion;
+    private imageId: string = '';
+
+    private readonly width = 160;
+    private offset = 0;
+
+    constructor(passion: Passion, tilesImageId: string) {
+        this.passion = passion;
+        this.imageId = tilesImageId;
+    }
+
+    update(dt: number) {
+        this.offset = this.offset - BACKGROUND_SPEED * dt;
+        if (this.offset < -this.width) {
+            this.offset += this.width;
+        }
+    }
+
+    draw() {
+        for (let i = 0; i < 3; i++) {
+            this.passion.graphics.blt(Math.ceil(this.offset + i * this.width), Math.ceil(15), this.imageId, 0, 176, this.width, 128);
+        }
+    }
+}
+
+class Ground {
+    private passion: Passion;
+    private imageId: string = '';
+
+    private readonly width = 48;
+    private offset = 0;
+
+    constructor(passion: Passion, tilesImageId: string) {
+        this.passion = passion;
+        this.imageId = tilesImageId;
+    }
+
+    update(dt: number) {
+        this.offset = this.offset - GROUND_SPEED * dt;
+        if (this.offset < -this.width) {
+            this.offset += this.width;
+        }
+    }
+
+    draw() {
+        for (let i = 0; i < 6; i++) {
+            this.passion.graphics.blt(Math.ceil(this.offset + i * this.width), Math.ceil(FLOOR_LEVEL), this.imageId, 208, 64, this.width, 47);
+        }
+    }
+}
+
+class Coin implements MaterialObject {
+    private passion: Passion;
+    private imageId: string = '';
+
+    readonly score = 10;
+    size: Position = new Position(16, 16);
+    pos: Position;
+
+    private animation: Animation;
+    private animationCollected: Animation;
+
+    isCollected = false;
+    private _isDead: boolean = false;
+
+    constructor(passion: Passion, tilesImageId: string, x: number, y: number) {
+        this.passion = passion;
+        this.imageId = tilesImageId;
+        this.pos = new Position(x, y);
+
+        const grid = new AnimationGrid(this.size.x, this.size.y);
+        this.animation = new Animation(grid.range('5-8', '8'), 0.1);
+        this.animationCollected = new Animation(grid.range('9-13', '8'), 0.1, false);
+    }
+
+    private get currentAnimation(): Animation {
+        return this.isCollected ? this.animationCollected : this.animation;
+    }
+
+    get isDead(): boolean {
+        return this._isDead;
+    }
+
+    private set isDead(value: boolean) {
+        this._isDead = value;
+    }
+
+    update(dt: number) {
+        this.currentAnimation.update(dt);
+        this.pos.x -= GROUND_SPEED * dt;
+
+        if ((this.isCollected && !this.currentAnimation.isPlaying()) ||
+            (this.pos.x + this.size.x < 0)) {
+            this.isDead = true;
+        }
+    }
+
+    draw() {
+        this.currentAnimation.draw(this.passion, Math.ceil(this.pos.x), Math.ceil(this.pos.y), this.imageId);
+    }
+}
+
+export class Example02 implements IGameExample {
+    private passion: Passion;
+
+    private tilesId: string = '';
+
+    private score: number = 0;
+
+    private player: Player | undefined;
+    private background: Background | undefined;
+    private ground: Ground | undefined;
+    private coins: Coin[] = [];
+
+    constructor(passion: Passion) {
+        this.passion = passion;
+    }
+
+    update(dt: number): void {
+        this.player?.update(dt);
+        this.background?.update(dt);
+        this.ground?.update(dt);
+        this.updateCoins(dt);
+    }
+
+    draw(): void {
+        this.passion.graphics.cls('#5580b3');
+
+        this.background?.draw();
+        this.ground?.draw();
+        this.coins.forEach(c => c.draw());
+        this.player?.draw();
+
+        this.drawScore();
+    }
+
+    onEnter() {
+        this.passion.system.init(220, 143, 'Example 02: Jump game');
+        this.tilesId = this.passion.resource.loadImage('/examples/arcade_by_GrafxKid.png');
+
+        this.player = new Player(this.passion);
+        this.background = new Background(this.passion, this.tilesId);
+        this.ground = new Ground(this.passion, this.tilesId);
+
+        this.coins.push(new Coin(this.passion, this.tilesId, 100, 100));
+    }
+
+    onLeave() {}
+
+    private drawScore() {
+        const textPos = new Position(7, 9);
+        const text = `SCORE: ${this.score}`;
+        this.passion.graphics.text(textPos.x + 1, textPos.y + 1, text, '#2d335c');
+        this.passion.graphics.text(textPos.x, textPos.y, text, '#eeeeee');
+    }
+
+    private updateCoins(dt: number) {
+        this.coins.forEach(c => {
+            c.update(dt);
+            if (this.player?.isCollidedWith(c) && !c.isCollected) {
+                c.isCollected = true;
+                this.score += c.score;
+            }
+        });
+        this.coins = this.coins.filter(c => !c.isDead);
+        if (this.coins.length === 0) {
+            this.coins.push(new Coin(this.passion, this.tilesId, this.passion.math.rndi(230, 360), this.passion.math.rndi(32, FLOOR_LEVEL - 16)));
+        }
+    }
+}
--- src/game/game.ts
+++ src/game/game.ts
@@ -1,5 +1,6 @@
 import type { Passion } from "../passion/passion";
 import { Example01 } from "./examples/01_hello_passion";
+import { Example02 } from "./examples/02_jump_game";
 import type { IGameExample } from "./examples/example";
 
 class Example {
@@ -25,31 +26,35 @@ export class Game {
         this.passion = passion;
         this.examples = [
             new Example(passion, 'Hello, Passion!', new Example01(this.passion)),
-            new Example(passion, 'Hello, Passion!', new Example01(this.passion)),
-            new Example(passion, 'Hello, Passion!', new Example01(this.passion)),
+            new Example(passion, 'Jump game [credits for sprites: GrafxKid]', new Example02(this.passion)),
         ];
 
         this.passion.system.init(420, 240, 'Passion examples');
+
+        this.currentExample = this.examples[1].example;
+        this.currentExample.onEnter();
     }
 
     update(dt: number) {
-        if (this.passion.input.btnp('ArrowDown')) {
-            this.exampleIndex += 1;
-            if (this.exampleIndex >= this.examples.length) {
-                this.exampleIndex = 0;
+        if (this.currentExample === undefined) {
+            if (this.passion.input.btnp('ArrowDown')) {
+                this.exampleIndex += 1;
+                if (this.exampleIndex >= this.examples.length) {
+                    this.exampleIndex = 0;
+                }
+            }
+            if (this.passion.input.btnp('ArrowUp')) {
+                this.exampleIndex -= 1;
+                if (this.exampleIndex < 0) {
+                    this.exampleIndex = this.examples.length - 1;
+                }
+            }
+            if ((this.passion.input.btnp('Enter') || this.passion.input.btnp('Space')) && this.currentExample === undefined) {
+                this.currentExample = this.examples[this.exampleIndex].example;
+                this.currentExample.onEnter();
             }
         }
-        if (this.passion.input.btnp('ArrowUp')) {
-            this.exampleIndex -= 1;
-            if (this.exampleIndex < 0) {
-                this.exampleIndex = this.examples.length - 1;
-            }
-        }
-        if ((this.passion.input.btnp('Enter') || this.passion.input.btnp('Space')) && this.currentExample === undefined) {
-            this.currentExample = this.examples[this.exampleIndex].example;
-            this.currentExample.onEnter();
-        }
-        if (this.passion.input.btnp('Escape') && this.currentExample !== undefined) {
+        else if (this.passion.input.btnp('Escape')) {
             this.currentExample.onLeave();
             this.currentExample = undefined;
             this.passion.system.init(420, 240, 'Passion examples');
