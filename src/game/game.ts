da02ca5bde40c523c687647934db800c343174b1
Dmitrii Eremin <erem.dmitrii@gmail.com>
2025-06-29 14:49:26 +0300

    Added example01

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,18 +1,83 @@
 import type { Passion } from "../passion/passion";
+import { Example01 } from "./examples/01_hello_passion";
+import type { IGameExample } from "./examples/example";
+
+class Example {
+    private passion: Passion;
+    public name: string;
+    public example: IGameExample;
+
+    constructor(passion: Passion, name: string, example: IGameExample) {
+        this.passion = passion;
+        this.name = name;
+        this.example = example;
+    }
+}
 
 export class Game {
     private passion: Passion;
 
+    private examples: Example[];
+    private exampleIndex: number = 0;
+    private currentExample: IGameExample | undefined = undefined;
+
     constructor(passion: Passion) {
         this.passion = passion;
-        this.passion.system.init(420, 240, 'Passion demo');
+        this.examples = [
+            new Example(passion, 'Hello, Passion!', new Example01(this.passion)),
+            new Example(passion, 'Hello, Passion!', new Example01(this.passion)),
+            new Example(passion, 'Hello, Passion!', new Example01(this.passion)),
+        ];
+
+        this.passion.system.init(420, 240, 'Passion examples');
     }
 
     update(dt: number) {
+        if (this.passion.input.btnp('ArrowDown')) {
+            this.exampleIndex += 1;
+            if (this.exampleIndex >= this.examples.length) {
+                this.exampleIndex = 0;
+            }
+        }
+        if (this.passion.input.btnp('ArrowUp')) {
+            this.exampleIndex -= 1;
+            if (this.exampleIndex < 0) {
+                this.exampleIndex = this.examples.length - 1;
+            }
+        }
+        if ((this.passion.input.btnp('Enter') || this.passion.input.btnp('Space')) && this.currentExample === undefined) {
+            this.currentExample = this.examples[this.exampleIndex].example;
+            this.currentExample.onEnter();
+        }
+        if (this.passion.input.btnp('Escape') && this.currentExample !== undefined) {
+            this.currentExample.onLeave();
+            this.currentExample = undefined;
+            this.passion.system.init(420, 240, 'Passion examples');
+        }
+
+        if (this.currentExample !== undefined) {
+            this.currentExample.update(dt);
+        }
     }
 
     draw() {
+        if (this.currentExample !== undefined) {
+            this.currentExample.draw();
+            return;
+        }
+
         this.passion.graphics.cls(1);
-        this.passion.graphics.text(10, 10, `FPS: ${this.passion.system.frame_count}`, 15);
+        this.passion.graphics.text(5, 4, 'Passion examples', 3);
+
+        let y = 24;
+        for (const [index, example] of this.examples.entries()) {
+            if (index === this.exampleIndex) {
+                this.passion.graphics.text(10, y, `>> ${index}: ${example.name} <<`, 7);
+            }
+            else {
+                this.passion.graphics.text(28, y, `${index}: ${example.name}`, 5);
+            }
+            y += 12;
+        }
     }
 }
