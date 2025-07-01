85d270a2fd3bced28ef26321fb71aaa0c2b8b622
Dmitrii Eremin <erem.dmitrii@gmail.com>
2025-07-01 23:41:49 +0300

    fix bdf font

--- src/game/examples/04_font_api.ts
+++ src/game/examples/04_font_api.ts
@@ -1,6 +1,6 @@
 import type { Passion } from "../../passion/passion";
 import type { IGameExample } from "./example";
-import { BIOS_FONT, BOLDER_FONT, OTHER_FONT } from "./utils/custom_fonts";
+import { BIOS_FONT } from "./utils/custom_fonts";
 
 export class Example04 implements IGameExample {
     private passion: Passion;
@@ -17,15 +17,16 @@ export class Example04 implements IGameExample {
 
         this.passion.graphics.cls(1);
 
-        this.passion.graphics.font();
-        this.passion.graphics.text(10, 10 + 35 * 0, testString, 7);
+        // this.passion.graphics.font();
+        // this.passion.graphics.text(10, 10 + 35 * 0, testString, 7);
 
-        this.passion.graphics.font(BOLDER_FONT);
-        this.passion.graphics.text(10, 10 + 35 * 1, testString, 7);
+        // this.passion.graphics.font(BOLDER_FONT);
+        // this.passion.graphics.text(10, 10 + 35 * 1, testString, 7);
 
-        this.passion.graphics.font(OTHER_FONT);
-        this.passion.graphics.text(10, 10 + 35 * 2, testString, 7);
+        // this.passion.graphics.font(OTHER_FONT);
+        // this.passion.graphics.text(10, 10 + 35 * 2, testString, 7);
 
+        this.passion.graphics.rect(10, 10 + 35 * 3, 100, 16, 0);
         this.passion.graphics.font(BIOS_FONT);
         this.passion.graphics.text(10, 10 + 35 * 3, testString, 7);
     }
